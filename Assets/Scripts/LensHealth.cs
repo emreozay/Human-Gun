735f34a7cd40b0cb4a072e570ab33f3afcb07500
Emre Özay <emreozay@mail.com>
2022-12-20 00:10:29 +0300

    Random lens health script added

---
+++ Assets/Scripts/LensHealth.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+
+public class LensHealth : MonoBehaviour
+{
+    [SerializeField]
+    private Material redLensMaterial;
+    [SerializeField]
+    private Material blueLensMaterial;
+
+    [SerializeField]
+    private MeshRenderer lensMesh;
+
+    [SerializeField]
+    private TextMeshPro lensText;
+
+    void Start()
+    {
+        SetLensHealth();
+    }
+
+    private void SetLensHealth()
+    {
+        int currentLevel = PlayerPrefs.GetInt("Level", 1);
+        int randomHealth = Random.Range(-currentLevel * 2, currentLevel * 2 + 1);
+
+        if (randomHealth < 0)
+        {
+            lensMesh.material = redLensMaterial;
+            lensText.text = randomHealth.ToString();
+        }
+        else if (randomHealth > 0)
+        {
+            lensMesh.material = blueLensMaterial;
+            lensText.text = "+" + randomHealth;
+        }
+        else
+        {
+            lensMesh.material = blueLensMaterial;
+            lensText.text = randomHealth.ToString();
+        }
+    }
+}
